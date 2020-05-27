cf648bbf09b1a712ce88a298644bdb02309224bb
xiewenhua <vievenvua@gmail.com>
2020-05-27 15:29:09 +0800

    hello user!Done

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello!!!"
+@app.route('/user/<name>')
+def hello(name):
+    return '<h1>Hello %s!</h1><img src="http://helloflask.com/totoro.gif">'%name
